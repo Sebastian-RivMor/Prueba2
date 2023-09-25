df308012c86998fbf099c0ae9a3220eb192a8c32
usuario <usuario@DESKTOP-HTGA6KE>
2023-09-25 09:25:07 -0500

    agregando git system

--- src/prueba/HOLAGIT.java
+++ src/prueba/HOLAGIT.java
@@ -4,10 +4,14 @@ package prueba;
 
 public class HOLAGIT {
     
-    public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println("¡Hola, Mundo!");
+        public class HolaMundo {
+        public static void main(String[] args) {
+            System.out.println("¡Hola, Mundo!");
+        }
+        
+        public static void git(String[] args) {
+            System.out.println("¡Hola, Git!");
+        }
     }
-}
 
 }
