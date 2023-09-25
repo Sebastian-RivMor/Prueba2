bd9df1399fd468b47de77ece9a975f2bf55610d7
usuario <usuario@DESKTOP-HTGA6KE>
2023-09-25 09:05:53 -0500

    Probando Hola mundo

---
+++ src/prueba/HOLAGIT.java
@@ -0,0 +1,13 @@
+
+package prueba;
+
+
+public class HOLAGIT {
+    
+    public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Mundo!");
+    }
+}
+
+}
